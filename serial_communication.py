bb5532ce3afe79ece4923a764acd22ffdcdc79ef
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-02 17:23:53 -0500

    Added code from example on Arduino website
    From this hopefully I will be able to reconfigure it to do what I want it to (i.e. push whatever I want from Python to the Arduino). This is the first step to real-time communication with the 128x64 OLED screen.

--- serial_communication.py
+++ serial_communication.py
@@ -1,5 +1,17 @@
 import serial
 import time
 
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1) 
+
+def write_read(x): 
+    arduino.write(bytes(x, 'utf-8')) 
+    time.sleep(0.05) 
+    data = arduino.readline() 
+    return data 
+
+
 if __name__ == "__main__":
-    pass
+    while True: 
+        num = input("Enter a number: ") # Taking input from user 
+        value = write_read(num) 
+        print(value) # printing the value 
