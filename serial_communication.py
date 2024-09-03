bb6c01b8ea545d3f247ca7505eb41d171c15c7e7
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-03 10:50:51 -0500

    Can push text to OLED successfully
    A user can type text into Python terminal and have it show up on the Adafruit 128x64 OLED screen.

--- serial_communication.py
+++ serial_communication.py
@@ -1,16 +1,17 @@
 import serial
 import time
 
-arduino = serial.Serial(port = 'COM3', baudrate = 115200, timeout = .1) 
+arduino = serial.Serial(port = 'COM3', baudrate = 115200, timeout = .1)
+maxOLEDCharacters = 21
 
-def write_read(x): 
-    arduino.write(bytes(x, 'utf-8')) 
-    time.sleep(0.05) 
-    data = arduino.readline() 
-    return data 
+def write_read(communication):
+    arduino.write(bytes(communication, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
 
 if __name__ == "__main__":
-    while True: 
-        quote = input("When is the winter of our discontent and by whom was it made glorious? ") # Taking input from user 
+    while True:
+        quote = input("What would you like the screen to say? ") # Taking input from user
         value = str(write_read(quote))
-        print(value) # printing the value 
+        print(value) # printing the value
--- serial_communication/serial_communication.ino
+++ serial_communication/serial_communication.ino
@@ -1,20 +1,53 @@
+// Libraries
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3D
+#define OLED_RESET 13
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
+
+const int COLOR = 1;
+
+String affirmative = "Job's done!"
+String marquee;
+// char* marquee;
+
 // int x;
 
-String response = "And all the clouds that lour'd upon our house in the deep bosom of the ocean buried.";
+// String response = "And all the clouds that lour'd upon our house in the deep bosom of the ocean buried.";
 
-void respond(){
-  String thing = Serial.readString();
+void display_text_OLED(String string){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(string);
+  display.display();
+};
+
+void push_to_display(String whatToPush){
+  marquee = Serial.readString();
+  display_text_OLED(whatToPush);
+  respond(affirmative);
+};
+
+void respond(String response){
   Serial.print(response);
 };
 
 void setup() {
   Serial.begin(115200);
+  display.begin();
   Serial.setTimeout(1);
+  display.clearDisplay();
 }
 
 void  loop() {
   while (!Serial.available());
-  respond();
-  // x = Serial.readString().toInt();
-  // Serial.print(x + 1);
+  marquee = Serial.readString();
+  push_to_display(marquee);
 }
