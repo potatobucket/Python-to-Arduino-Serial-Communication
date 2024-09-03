263b5f8d6e11c2b3656acbcaf32acbacc74ebc47
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-02 19:49:07 -0500

    Cleanup
    Fixed the port for the Arduino and just general cleanup.

--- serial_communication.py
+++ serial_communication.py
@@ -1,14 +1,13 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1) 
+arduino = serial.Serial(port = 'COM3', baudrate = 115200, timeout = .1) 
 
 def write_read(x): 
     arduino.write(bytes(x, 'utf-8')) 
     time.sleep(0.05) 
     data = arduino.readline() 
     return data 
-
 
 if __name__ == "__main__":
     while True: 
