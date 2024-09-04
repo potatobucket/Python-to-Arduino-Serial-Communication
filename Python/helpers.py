a8784de0976d33e3e37b037c686893c9c581c317
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-04 15:17:55 -0500

    Mostly correct
    It now correctly formats the FIRST word that is too long to fit on one line of the OLED screen but words that come afterward seem to be stuck somewhere. I'll figure this out somehow.

--- Python/helpers.py
+++ Python/helpers.py
@@ -31,18 +31,26 @@ class Text:
         stringChunk: str = ""
         chunkSize: int = 0
         for index, word in enumerate(splitString):
+            splitWords: list = []
             if is_word_too_large(word, self.maxCharacters - 1) == True:
                 splitLargeWord = split_large_word(word, self.maxCharacters - 2)
-                stringArray.append(splitLargeWord[0]) #-- <this bit right here is where it all gets hecked up. I just need to refine when it happens (which should be later)
+                splitWords.append(splitLargeWord[0]) #-- <this seems to be the answer to the problem. We'll see!
                 while is_word_too_large(splitLargeWord[1], self.maxCharacters - 1) == True:
                     splitLargeWord = split_large_word(splitLargeWord[1], self.maxCharacters - 2)
-                    stringArray.append(splitLargeWord[0])
+                    splitWords.append(splitLargeWord[0])
                 word = splitLargeWord[1]
             chunkSize = len(stringChunk)
             if chunkSize + len(word) < self.maxCharacters:
                 stringChunk += word
             elif index < len(splitString) - 1:
                 stringArray.append(stringChunk)
+                if len(splitWords) > 0:
+                    # stringArray.append(stringChunk)
+                    # stringChunk = ""
+                    # chunkSize = 0
+                    for thing in splitWords:
+                        stringArray.append(thing)
+                    splitWords.clear()
                 stringChunk = ""
                 chunkSize = 0
                 stringChunk += word
@@ -65,27 +73,27 @@ class Text:
 #-- Ergo this little note I'm leaving for myself.
 #-- TODO: delete after figuring it out. This section used only for testing
 if __name__ == "__main__":
-    bigWord = "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenu-akitanatahu"  
-    text = Text(bigWord)                                #-- prints: Taumatawhakatangihan-                                                                     
-    for i in text.word_wrap:                            #---------: gakoauauotamateaturi-                                                                     
-        print(f"{i:22}: {len(i)}")                      #---------: pukakapikimaungahoro-                                                                     
-                                                        #---------: nukupokaiwhenu-akita-                                                                     
-                                                        #---------: natahu
+    # bigWord = "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenu-akitanatahu"  
+    # text = Text(bigWord)                                #-- prints: Taumatawhakatangihan-                                                                     
+    # for i in text.word_wrap:                            #---------: gakoauauotamateaturi-                                                                     
+    #     print(f"{i:22}: {len(i)}")                      #---------: pukakapikimaungahoro-                                                                     
+    #                                                     #---------: nukupokaiwhenu-akita-                                                                     
+    #                                                     #---------: natahu
     
-    bleh = Text("When in the Course of human events, it becomes necessary for one people to dissolve the antidisestablishmentarianism bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation. We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.")
+    bleh = Text("When in the Course of human events, it becomes necessary for one people to dissolve the antidisestablishmentarianism bands which have connected them with another, and to assume among the powers of the earth, the separate and equal Floccinaucinihilipilification to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation. We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.")
     for i,v in enumerate(bleh.word_wrap):               #-- prints: When in the Course
         print(f"{i:>3}", f"{v:23}", f"{len(v):>2}")     #---------: of human events, it
                                                         #---------: becomes necessary
                                                         #---------: for one people to
-                                                        #---------: antidisestablishment- <- wrong
                                                         #---------: dissolve the
-                                                        #---------: arianism bands which  <- wrong
+                                                        #---------: antidisestablishment- <- Now correct
+                                                        #---------: arianism bands which  <- Now correct
                                                         #---------: have connected them
                                                         #---------: with another, and to
                                                         #---------: assume among the
                                                         #---------: powers of the earth,
                                                         #---------: the separate and
-                                                        #---------: equal station to
+                                                        #---------: equal ification to    <- However, now this one is incorrect
                                                         #---------: which the Laws of
                                                         #---------: Nature and of
                                                         #---------: Nature's God entitle
